694d9e353989f13ce9fbe333398b68af568afe56
Shivam Badhopulu <shivambadhopulu@gmail.com>
2024-10-29 00:38:36 +0530

    first Task First Commit

---
+++ TASK1-AI/index.py
@@ -0,0 +1,9 @@
+import re
+import random
+
+greetings=["hello", "hi", "hey", "greetings", "what's up"]
+goodbyes=["goodbye", "bye", "see you later", "quit","exit"]
+feeling_questions=["how are you", "how are you doing", "how are you feeling"]
+help_keywords=["help", "support", "assist", "aid", "guidance"]
+weather_keywords = ["weather", "temperature", "forecast"]
+identity_questions = ["who are you", "what are you", "introduce yourself"]
