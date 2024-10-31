f36acfd4e9f0de1a32f7c958a1fa5f2a57aa4b41
Shivam Badhopulu <shivambadhopulu@gmail.com>
2024-10-31 21:07:50 +0530

    second Task first Commit

---
+++ TASK2-TICTACTOE/board.py
@@ -0,0 +1,18 @@
+# Board Styling
+def check_winner(board):
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != '':                 #Score-and-Winner
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] != '':
+            return board[0][i]
+    if board[0][0] == board[1][1] == board[2][2] != '':
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != '':                 
+        return board[0][2]
+    return None
+
+def is_board_full(board):
+    return all(cell != '' for row in board for cell in row)         #Checking-Before-Resetting
+
+def reset_board():
+    return [['' for _ in range(3)] for _ in range(3)]       #Resetting
